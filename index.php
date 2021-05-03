04a17a689f9e243b4499e7d3ad6803162fcc37dc
Richard <richard.yang.tong@gmail.com>
2021-05-03 17:43:14 -0400

    Added dynamic rendered php code.

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-echo 'Hello, ninjas';
+//echo 'Hello, ninjas';
 
 ?>
 
@@ -11,11 +11,11 @@ echo 'Hello, ninjas';
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>My first PHP file</title>
 </head>
 
 <body>
-
+  <h1><?php echo 'Hello, ninjas!' ?></h1>
 </body>
 
 </html>
